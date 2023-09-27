93732ea348701e134bbbccfcf81f5c7b64633c75
puneeeeeeet <puneetchhabra1607@gmail.com>
2023-09-27 22:22:40 +0530

    first commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+import { DESTRUCTION } from "dns"
+import { Ghost } from "lucide-react"
+export default function Home() {
+  return (
+    <div className="">
+      This is a protected route
+    </div>  
+  )
+}
